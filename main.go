19e835b5c72699ab981fb61be0162b74e2b5c3ce
megharosejayan <megharose15@gmail.com>
2022-12-09 17:49:48 +0900

    post/create data endpoint

--- main.go
+++ main.go
@@ -24,8 +24,21 @@ func getBooks(c *gin.Context) {
 
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+
+}
+
 func main() {
 	router := gin.Default() //to handle different routes
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
